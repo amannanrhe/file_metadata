d8d3422c478a907b81bc5acadffcc4ba13d1d6b1
aman <amannanrhe@gmail.com>
2018-01-20 01:48:05 -0700

    POST Req handling
    Handle POST req on form submit with object and return size in bytes

--- routes/ApiController.js
+++ routes/ApiController.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var multer  = require('multer');
+var upload = multer({dest: path.resolve(__dirname, '../uploads/')});
 
 router.get('/', function(req, res, next){
     res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });
 
-router.post('/upload', function(req, res, next){
-    
+router.post('/upload', upload.single('uploadata'), function(req, res, next){
+    res.send({size:req.file.size + ' bytes'});
 });
 
 module.exports = router;
